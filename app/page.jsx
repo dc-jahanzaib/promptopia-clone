1602d88d7b6c1268a271e13e397611892098427e
dax-jahanzaib <jhussain@digitalauxilius.com>
2023-08-16 16:57:16 +0500

    initialize

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Feed from "@components/Feed"
+
+const Home = () => {
+  return (
+    <>  
+      <div>Home est</div>
+      <Feed />
+    </>
+  )
+}
+
+export default Home
---
+++ components/Feed.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Feed = () => {
+  return (
+    <div>Feed Component</div>
+  )
+}
+
+export default Feed
